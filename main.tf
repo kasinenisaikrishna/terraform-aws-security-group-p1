55a6914901fdf15f136aa8e0b14527d93f10f9a3
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-08 16:42:44 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ resource "aws_security_group" "main" {
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = var.vpc_id
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = merge(
     var.common_tags,
     var.sg_tags,
